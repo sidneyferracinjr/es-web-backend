3ee8311cfedb91f26ddddb33bd356c6e8359d905
sidneyferracinjr <sidneyferracinjr@gmail.com>
2024-11-05 15:20:11 -0300

    todolist

---
+++ todolist/validators/TaskValidator.js
@@ -0,0 +1,13 @@
+const Joi = require("joi")
+
+const TaskSchema = Joi.object({
+    id: Joi.number()
+        .integer()
+        .greater(0),
+    nome: Joi.string()
+        .min(3)
+        .max(30)
+        .required()
+}).with("id", "nome")
+
+module.exports = TaskSchema
